1069aaafef03bd9fa431d2f2f077e138d62ffe88
Katchawww <mathiasgomes0103@gmail.com>
2026-05-10 23:12:15 +0200

    Add files via upload

--- html/fonctions.php
+++ html/fonctions.php
@@ -12,4 +12,40 @@ function readData($file){
 function saveData($file, $data){
     file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
 }
+
+
+
+
+
+function checkBlocked(){
+
+$users =
+readData("json/utilisateurs.json");
+
+
+
+foreach($users as $u){
+
+    if(
+        isset($_SESSION["user"])
+        &&
+        $u["login"] ==
+        $_SESSION["user"]["login"]
+    ){
+
+        if(
+            isset($u["blocked"])
+            &&
+            $u["blocked"]
+        ){
+
+            session_destroy();
+
+            header("Location: connexion.php");
+
+            exit;
+        }
+    }
+}
+}
 ?>
